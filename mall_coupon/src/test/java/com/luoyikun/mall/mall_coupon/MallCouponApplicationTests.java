38a56e30c7dba66e4535041cc5d60cf51976aed8
Y3ko <luoyikun19@19.com>
2022-07-05 22:22:25 +0800

    项目初始创建

---
+++ mall_coupon/src/test/java/com/luoyikun/mall/mall_coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoyikun.mall.mall_coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall_ware/src/test/java/com/luoyikun/mall/mall_ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoyikun.mall.mall_ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
